b117ec40d27a1784f41008cd621a895a67a1fe30
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2023-08-30 16:56:54 -0300

    feature: add route for doc swagger

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,16 @@
 import express, { Request, Response } from 'express'
 import dotenv from 'dotenv'
+import swaggerUi from 'swagger-ui-express'
 import router from './routes/Routes'
+import swaggerFile from '../swagger.json'
 
 dotenv.config()
 
 const app = express()
 app.use(express.json())
 
-app.get('/', (req: Request, res: Response) => {
-  return res.status(200).send({
-    response: 'Express TypeScript',
-  })
-})
-
 app.use(router)
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.listen(process.env.APP_PORT, () => {
   console.log(`${process.env.APP_NAME} running on port ${process.env.APP_PORT}`)
