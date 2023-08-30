9e98762669105d084c7800d370aab6b2d312593e
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2023-08-30 16:35:26 -0300

    feature: add crud registrations

--- src/controllers/PersonsController.ts
+++ src/controllers/PersonsController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import Helper from '../helper/Helper'
 import Persons, { PersonAttributes } from '../db/models/Persons'
+import Registrations from '../db/models/Registrations'
 
 class PersonController {
   static async getAll(req: Request, res: Response): Promise<Response> {
@@ -9,7 +10,7 @@ class PersonController {
 
       return res
         .status(200)
-        .send(Helper.ResponseData(200, 'Getted', null, allPersons))
+        .send(Helper.ResponseData(200, null, null, allPersons))
     } catch (error) {
       console.log(error)
 
@@ -23,7 +24,7 @@ class PersonController {
       const onePerson: PersonAttributes | null = await Persons.findByPk(id)
       return res
         .status(200)
-        .send(Helper.ResponseData(200, 'Getted', null, onePerson))
+        .send(Helper.ResponseData(200, null, null, onePerson))
     } catch (error) {
       return res.status(500).send(Helper.ResponseData(500, '', error, null))
     }
@@ -37,7 +38,7 @@ class PersonController {
 
       return res
         .status(201)
-        .send(Helper.ResponseData(201, 'Created', null, newPersonCreated))
+        .send(Helper.ResponseData(201, null, null, newPersonCreated))
     } catch (error) {
       return res.status(500).send(Helper.ResponseData(500, '', error, null))
     }
@@ -55,7 +56,7 @@ class PersonController {
 
       return res
         .status(200)
-        .send(Helper.ResponseData(200, 'Updated', null, personUpdated))
+        .send(Helper.ResponseData(200, null, null, personUpdated))
     } catch (error) {
       return res.status(500).send(Helper.ResponseData(500, '', error, null))
     }
@@ -66,11 +67,73 @@ class PersonController {
 
     try {
       await Persons.destroy({ where: { id: Number(id) } })
-      return res.status(200).send(
-        Helper.ResponseData(200, 'Deleted', null, {
-          message: 'deleted successfully',
-        })
-      )
+      return res
+        .status(204)
+        .send(Helper.ResponseData(204, 'Deleted successfully', null, null))
+    } catch (error) {
+      return res.status(500).send(Helper.ResponseData(500, '', error, null))
+    }
+  }
+
+  static async getOneRegistration(req: Request, res: Response) {
+    const { studentId, registrationId } = req.params
+
+    try {
+      const oneRegistration = await Registrations.findOne({
+        where: { id: Number(registrationId), student_id: Number(studentId) },
+      })
+
+      return res
+        .status(201)
+        .send(Helper.ResponseData(200, null, null, oneRegistration))
+    } catch (error) {
+      return res.status(500).send(Helper.ResponseData(500, '', error, null))
+    }
+  }
+
+  static async createRegistration(req: Request, res: Response) {
+    const { studentId } = req.params
+    const newRegistration = { student_id: studentId, ...req.body }
+
+    try {
+      const newRegistrationCreated = await Registrations.create(newRegistration)
+
+      return res
+        .status(201)
+        .send(Helper.ResponseData(201, null, null, newRegistrationCreated))
+    } catch (error) {
+      return res.status(500).send(Helper.ResponseData(500, '', error, null))
+    }
+  }
+
+  static async updateRegistration(req: Request, res: Response) {
+    const { studentId, registrationId } = req.params
+    const newDataRegistration = req.body
+
+    try {
+      await Registrations.update(newDataRegistration, {
+        where: { id: Number(registrationId), student_id: Number(studentId) },
+      })
+      const registrationUpdated = await Registrations.findByPk(registrationId)
+
+      return res
+        .status(200)
+        .send(Helper.ResponseData(200, null, null, registrationUpdated))
+    } catch (error) {
+      return res.status(500).send(Helper.ResponseData(500, '', error, null))
+    }
+  }
+
+  static async deleteRegistration(req: Request, res: Response) {
+    const { registrationId } = req.params
+
+    try {
+      await Registrations.destroy({
+        where: { id: Number(registrationId) },
+      })
+      return res
+        .status(204)
+        .send(Helper.ResponseData(204, 'Deleted successfully', null, null))
     } catch (error) {
       return res.status(500).send(Helper.ResponseData(500, '', error, null))
     }
---
+++ src/db/migrations/20230830191651-create-registrations.js
@@ -0,0 +1,44 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('registrations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      status: {
+        type: Sequelize.STRING,
+      },
+      class_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'classes',
+          key: 'id',
+        },
+      },
+      student_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'persons',
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('registrations')
+  },
+}
