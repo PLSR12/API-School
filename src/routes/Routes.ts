6cb7e695d460e8f314fa277496e0743285edf83f
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2023-08-30 16:36:25 -0300

    feature: update routes

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,19 +1,45 @@
 import express from 'express'
-import PersonController from '../controllers/PersonsController'
+import PersonController from '../controllers/personsController'
 import LevelController from '../controllers/levelsController'
+import ClassController from '../controllers/classesController'
 
 const router = express.Router()
 
+// PERSONS
 router.get('/persons', PersonController.getAll)
 router.get('/persons/:id', PersonController.getOne)
 router.post('/persons', PersonController.create)
 router.put('/persons/:id', PersonController.update)
 router.delete('/persons/:id', PersonController.delete)
+router.get(
+  '/persons/:studentId/registration/:registrationId',
+  PersonController.getOneRegistration
+)
+router.post(
+  '/persons/:studentId/registration',
+  PersonController.createRegistration
+)
+router.put(
+  '/persons/:studentId/registration/:registrationId',
+  PersonController.updateRegistration
+)
+router.delete(
+  '/persons/:studentId/registration/:registrationId',
+  PersonController.deleteRegistration
+)
 
+// LEVELS
 router.get('/levels', LevelController.getAll)
 router.get('/levels/:id', LevelController.getOne)
 router.post('/levels', LevelController.create)
 router.put('/levels/:id', LevelController.update)
 router.delete('/levels/:id', LevelController.delete)
+
+// CLASSES
+router.get('/classes', ClassController.getAll)
+router.get('/classes/:id', ClassController.getOne)
+router.post('/classes', ClassController.create)
+router.put('/classes/:id', ClassController.update)
+router.delete('/classes/:id', ClassController.delete)
 
 export default router
